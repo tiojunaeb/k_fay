00bc3fc689209a462803e30ce29935e9b6273f08
tiojunaeb <benjarias50@gmail.com>
2023-11-20 19:04:38 -0300

    ajuste-detalle

--- lib/pages/DetallePage.dart
+++ lib/pages/DetallePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class DetallePage extends StatelessWidget {
   const DetallePage({super.key});
@@ -10,29 +11,62 @@ class DetallePage extends StatelessWidget {
         title: Text('Detalle evento'),
       ),
       body: Column(
+        
         children: [
           Container(
             width: 360,
             height: 360,
             child: Image.asset('assets/paes.png'),
           ),
-          Row(
-            
-            children: [
-              Container(
-                padding:EdgeInsets.all(8) ,
-                child: Text('Ubicacion: Valpo', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                )),
-                Spacer(),
-                Container(
-                padding:EdgeInsets.all(8) ,
-                child: Text('fecha: 12/12/2023', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ))
-            ],
+          Container(child: 
+          Text('Adios paes', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
           ),
           Row(
-            children: [],
+           children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Icon(MdiIcons.calendar, size: 50,),
+              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text('jueves, noviembre 23, 2023', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+              Text('Hora: 20:30-5:00')])
+           ],),
+           Container(
+            child: Row(children: [
+              Container(child: 
+              Icon(MdiIcons.mapMarker, size: 50,)),
+              Column(
+                children: [
+                  Text('Viña del mar', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                  Text('Hotel sheraton'),
+                ],
+              )
+              
+            ]
+            ),
+           ),
+          Row(
+           children: [
+             Icon(MdiIcons.heart, size: 50,),
+             Column(children: [
+              Text('  0 ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30))
+             ],)
+           ],
+          ),
+          Spacer(),
+          Row(
+            children: [Expanded(child: 
+            Padding(padding: EdgeInsets.all(8), child: ElevatedButton(onPressed: (){}, child: Text('Guardar evento'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.black),
+              foregroundColor: MaterialStateProperty.all(Colors.white)
+            ),),)
+            )],
           )
+
+          
         ],
       )
     );
